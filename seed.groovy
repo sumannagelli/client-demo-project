b7d4b9184f30d7e92ff3a59e9c3f4f956f95c022
NagireddyGuduru <Guduru.Reddy@mphasis.com>
2017-09-07 18:21:42 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,25 +1,4 @@
-def propeties = new Properties()
-streamFileFromWorkspace('./seed.properties').withStream {
-  InputStream it -> properties.load(it)
-}
 job("${SEED_PROJECT}-${SEED_BRANCH}-build") {
    description "Building the ${BRANCH} branch."
-   parameters {
-      stringParam('COMMIT', 'HEAD', 'Commit to build')
-   }
-   scm {
-      git {
-         remote {
-             url PROJECT_SCM_URL
-             branch "origin/${BRANCH}"
-         }
-         extensions {
-             wipeOutWorkspace()
-             localBranch BRANCH
-         }
-      }
-   }
-   steps {
-      shell "Look! I'm building ${BRANCH}!"
-   }
+  
 }
